e74e79e843edf093e11f0e4fc7712f1a98ec8f40
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2023-11-01 10:10:57 -0400

    not working game logic with oled

--- GameLogic/GameLogic.ino
+++ GameLogic/GameLogic.ino
@@ -1,11 +1,24 @@
 #include <time.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define START_BUTTON 4
 #define LED1 6
 #define LED2 7
 #define LED3 8
 
+#define WIDTH 128
+#define HEIGHT 64
+
+Adafruit_SSD1306 display(WIDTH, HEIGHT, &Wire, -1);
+
 enum commands {TwistIt, PourIt, RipIt};
+
+// initialize input time and score
+float inputTime = 2;
+int score = 0;
 
 int rand_seed_counter;
 
@@ -23,14 +36,31 @@ void setup() {
   // start button
   pinMode(START_BUTTON, INPUT);
 
+  // setup OLED display
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    while(true);
+  }
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
 }
 
-void wait_for_user_response(uint8_t input_pin, uint8_t signal_to_wait_for, uint8_t output_pin) {
-  digitalWrite(output_pin, HIGH);
-  while((digitalRead(input_pin) != signal_to_wait_for));
+void wait_for_user_response(uint8_t input_pin, uint8_t signal_to_wait_for, uint8_t output_pin, String command) {
+  //digitalWrite(output_pin, HIGH);
+  
+  // write command to OLED
+  display.clearDisplay();
+  display.println(score);
+  display.print(command);
+  display.display();
+
+  while(digitalRead(input_pin) != signal_to_wait_for);
   while(digitalRead(input_pin) == signal_to_wait_for); // wait for signal to reset
 
-  digitalWrite(output_pin, LOW);
+  //digitalWrite(output_pin, LOW);
 
 }
 
@@ -39,15 +69,16 @@ void loop() {
   // keep increasing the rand seed counter until start is pressed. This will add a randomness effect
   // because we don't have an RTC to keep track of time.
   while(digitalRead(START_BUTTON) == HIGH) {
+    display.print("waiting...");
+    display.display();
+    display.clearDisplay();
     rand_seed_counter++;
   }
 
+  display.clearDisplay();
+
   // seed random numbers
     srand(rand_seed_counter);
-
-    // initialize input time and score
-    int inputTime = 2;
-    int score = 0;
 
     // loop for the game
     bool isRunning = true;
@@ -56,12 +87,11 @@ void loop() {
     else { // game is running
         while (true) {
             int command = rand() % 3;
-            printf("Score: %d\n", score);
 
             // twist it
             if (command == TwistIt) {
                 //printf("Twist It!\n");
-                wait_for_user_response(START_BUTTON, LOW, LED1);
+                wait_for_user_response(START_BUTTON, LOW, LED1, "Twist It!");
 
                 // poll for user input
                 delay(inputTime*1000);
@@ -73,7 +103,7 @@ void loop() {
             // pour it
             else if (command == PourIt) {
                 //printf("Pour It!\n");
-                wait_for_user_response(START_BUTTON, LOW, LED2);
+                wait_for_user_response(START_BUTTON, LOW, LED2, "Pour It!");
 
                 // poll for user input
                 delay(inputTime*1000);
@@ -86,7 +116,7 @@ void loop() {
             // rip it
             else if (command == RipIt) {
                 //printf("Rip It!\n");
-                wait_for_user_response(START_BUTTON, LOW, LED3);
+                wait_for_user_response(START_BUTTON, LOW, LED3, "Rip It!");
 
                 // poll for user input
                 delay(inputTime*1000);
