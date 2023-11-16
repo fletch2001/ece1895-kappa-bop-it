ba48c0e03d75af746d965f2887eef32db68aaf09
Jacob Kerstetter <jacob@Jacobs-MacBook-Pro.local>
2023-11-16 16:50:54 -0500

    worked on updating documentation

--- GameLogic/GameLogic.ino
+++ GameLogic/GameLogic.ino
@@ -1,3 +1,4 @@
+// includes for libraries used
 #include <time.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -5,100 +6,91 @@
 #include <Adafruit_SSD1306.h>
 #include "MPU6050.h"
 
-
+// defines for pins for inputs
 #define START_BUTTON 5
-#define LED1 6
-#define LED2 7
-#define LED3 8
 #define RIP_IT_SLIDE_POT 19
 #define TWIST_IT_ROT_POT 17
 
-#define TEST_BUTTON 15
+// define for speaker output
+#define SPEAKER 15
 
+// defines for input commands
 #define TWIST_IT 0
 #define RIP_IT 1
 #define POUR_IT 2
 
-#define WIDTH 128
-#define HEIGHT 64
-
-#define UPRIGHT_DIRECTION Z
-
 String commands_list[] = {"Twist it!", "Rip it!", "Pour it!"};
-
-
-int PREV_TWIST_IT;
-int PREV_RIP_IT;
-
-Adafruit_SSD1306 display(WIDTH, HEIGHT, &Wire, -1);
-MPU6050 accelgyro; // IMU object
-
 enum commands {TwistIt, RipIt, PourIt};
 int current_command;
 
-// initialize input time and score
-float inputTime = 2;
-int score = 0;
+// defines for OLED display
+#define WIDTH 128
+#define HEIGHT 64
 
-int rand_seed_counter;
+// defines for accelerometer
+#define UPRIGHT_DIRECTION Z
 
+// global vars for previous potentiometer inputs
+int PREV_TWIST_IT;
+int PREV_RIP_IT;
 
+// initialize OLED display and IMU
+Adafruit_SSD1306 display(WIDTH, HEIGHT, &Wire, -1);
+MPU6050 accelgyro; 
 int16_t ax, ay, az;
 
+// global vars for input time, score, and random seed
+float inputTime = 2;
+int score = 0;
+int rand_seed_counter;
+
 void setup() {
-  // set up serial output to test code
-  Serial.begin(9600);
-
   rand_seed_counter = 0;
-
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
 
   // start button
   pinMode(START_BUTTON, INPUT);
-  pinMode(TEST_BUTTON, INPUT);
 
-  // setup OLED display
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    while(true);
-  }
+  // initialize accelgyro
+  accelgyro.initialize();
 
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-
-  accelgyro.initialize(); // init accelgyro
-
+  // initialize current potentiometer values as the baseline
   PREV_TWIST_IT = analogRead(TWIST_IT_ROT_POT);
   PREV_RIP_IT = analogRead(RIP_IT_SLIDE_POT);
 
+  // if OLED setup fails, program will hang
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) while(true);
+
+  // set default OLED writing settings
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
 }
 
+// function to write score to OLED display
 void display_score_to_oled() {
+  // set cursor to first line and write score
   display.setCursor(0, 0);
   display.setTextSize(1);
   display.print("score: " + String(score));
   display.display();
 }
 
+// function to display command to OLED display
 void display_command_to_oled() {
-  // second line
+  // set cursor to second line and write command
   display.setCursor(0, 10);
   display.setTextSize(2);
   display.print(commands_list[current_command]);
   display.display();
 }
 
-void display_command_and_score_to_oled(String command) {
-  // write command to OLED
+// wrapper function to write score and command
+void display_command_and_score_to_oled() {
   display.clearDisplay();
   display_score_to_oled();
   display_command_to_oled();
 }
 
+// functions to poll the sensors
 // the 1,2,4 method - no combination of them equals another and then we can distinguish between outputs passed back.
 int poll_twist_it() {
   if(analogRead(TWIST_IT_ROT_POT) != PREV_TWIST_IT) {
@@ -149,18 +141,17 @@ int poll_sensors() {
   return poll_pour_it() + poll_rip_it() + poll_twist_it();
 }
 
+// function to poll sensors and check if the user responded within the time limit
 void wait_for_user_response(int command) {  
+  // set current command display it
   current_command = command;
-  
-  display_command_and_score_to_oled(commands_list[command]);
+  display_command_and_score_to_oled();
 
-  // accelgyro.getAcceleration(&prev_ax, &prev_ay, &prev_az);
-
+  // initialize the start time of the command being sent
   int timeStart = millis();
   int sensor_sum = poll_sensors();
 
   // wait for input to go to desired and then back to normal state
-  
   int timeAction = timeStart;
   while(!sensor_sum) {
     // constantly poll time elapsed
@@ -168,6 +159,7 @@ void wait_for_user_response(int command) {
 
     // if time elapsed is longer than current input time allowed, game over!
     if ((timeAction - timeStart) > inputTime*1000) {
+      // display score and game over message
       display.clearDisplay();
       display.setCursor(0, 0);
       display.setTextSize(1);
@@ -176,29 +168,38 @@ void wait_for_user_response(int command) {
       display.setTextSize(2);
       display.print("GAME OVER!");
       display.display();
-      // hang here
+      
+      // hang at game over state
       while(1);
     }
+
+    // repoll sensors to see if input is correct
     sensor_sum = poll_sensors();
   }
 
   // left shift command for 1-2-4 mapping
+  // if the sensor response is not the correct command, game over!
   if(sensor_sum != (command << 1)) {
+    // display score and game over message
     display.clearDisplay();
-      display.setCursor(0, 0);
-      display.setTextSize(1);
-      display.print("score = " + String(score) + "\n");
-      display.setCursor(0, 10);
-      display.setTextSize(2);
-      display.print("GAME OVER!");
-      display.display();
-      while(1);
-  } else {
+    display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.print("score = " + String(score) + "\n");
+    display.setCursor(0, 10);
+    display.setTextSize(2);
+    display.print("GAME OVER!");
+    display.display();
+
+    // hang at game over state
+    while(1);
+  } 
+  
+  // if input was correct, increase score and decrease allowed input time
+  else {
     score++;
     inputTime -= 0.02;
     display_score_to_oled();
   }
-  
 }
 
 void loop() {
@@ -213,13 +214,9 @@ void loop() {
     display.clearDisplay();
     rand_seed_counter++;
   }
-  
-  display.print("Game starting!");
-      display.display();
 
   // wait for button to be let-go of
   while(digitalRead(START_BUTTON) == HIGH);
-
   display.clearDisplay();
 
   // seed random numbers
@@ -227,52 +224,36 @@ void loop() {
 
   // loop for the game
   bool isRunning = true;
-  if (!isRunning) {// game has not started (ie. button needs to be pressed)
+  if (!isRunning) { // game has not started (ie. button needs to be pressed)
       display.setCursor(0, 0);
       display.print("game is not running");
       display.display();
-  } else { // game is running
-      
-      while (true) {
-        //int command = rand() % 3;
-        int command = POUR_IT;
-        // twist it
-        if (command == TwistIt) {
-            wait_for_user_response(TWIST_IT);
+  } 
+  
+  // game is running - run game loop
+  else {
+    while (true) {
+      // get a random command
+      int command = rand() % 3;
 
-            // poll for user input
-            delay(1500);
-
-            // adjust inputTime to be faster for next instruction
-            // inputTime -= 0.02;
-            // score++;
-        }
-        // pour it
-        else if (command == PourIt) {
-            //printf("Pour It!\n");
-
-            wait_for_user_response(POUR_IT);
-
-            // poll for user input
-            delay(1500);
-
-            // adjust inputTime to be faster for next instruction
-            // inputTime -= 0.02;
-            // score++;
-        }
-
-        // rip it
-        else if (command == RipIt) {
-            //printf("Rip It!\n");
-            wait_for_user_response(RIP_IT);
-
-            // poll for user input
-            delay(1500);
-
-            // adjust inputTime to be faster for next instruction
-            // inputTime -= 0.02;
-            // score++;
-        }
+      // twist it
+      if (command == TwistIt) {
+        // poll for user input
+        wait_for_user_response(TWIST_IT);
+        delay(1000);
       }
+      // pour it
+      else if (command == PourIt) {
+        // poll for user input
+        wait_for_user_response(POUR_IT);
+        delay(1000);    
+      }
+      // rip it
+      else if (command == RipIt) {
+          // poll for user input
+          wait_for_user_response(RIP_IT);    
+          delay(1000);
+      }
+    }
   }
 }
