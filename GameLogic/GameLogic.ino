20ba08448c57f431c22f8333aa8516f3ee1b6252
Noah Lichstein <noahlichstein@gmail.com>
2023-12-03 17:07:11 -0500

    it all works

--- GameLogic/GameLogic.ino
+++ GameLogic/GameLogic.ino
@@ -6,14 +6,14 @@
 #include <math.h>
 
 
-//#define DEBUG 1
+//#define DEBUG
 
 #define MPU_ADDR 0x68
 
 // defines for pins for inputs
 #define START_BUTTON 5
 #define RIP_IT_SLIDE_POT A2
-#define TWIST_IT_ROT_POT 17
+#define TWIST_IT_ROT_POT A3
 
 // define for speaker output
 #define SPEAKER 9
@@ -53,6 +53,8 @@ int current_command;
 // global vars for previous potentiometer inputs
 int PREV_TWIST_IT;
 int PREV_RIP_IT;
+int RIP_IT_VAL;
+int TWIST_IT_VAL;
 #define TOLERANCE 75
 
 // initialize OLED display and IMU
@@ -71,6 +73,12 @@ double X_ANG;
 double Y_ANG;
 double Z_ANG;
 
+int SENSOR_SUM;
+int RIP_IT_DIFF;
+int TWIST_IT_DIFF;
+int TIMESTART;
+int TIMEACTION;
+
 // initialize file for sd card
 // File sdCard;
 // TMRpcm tmrpcm;
@@ -81,17 +89,27 @@ void setup() {
     // start button
     pinMode(START_BUTTON, INPUT);
 
-    // initialize accelgyro
-    mpu.begin();
-
     // initialize current potentiometer values as the baseline
-    PREV_TWIST_IT = map(analogRead(TWIST_IT_ROT_POT), 0, 1023, 0, 179);
-    PREV_RIP_IT = map(analogRead(RIP_IT_SLIDE_POT), 0, 1023, 0, 179);
+    // PREV_TWIST_IT = map(analogRead(TWIST_IT_ROT_POT), 0, 1023, 0, 179);
+    // PREV_RIP_IT = map(analogRead(RIP_IT_SLIDE_POT), 0, 1023, 0, 179);
 
     // if OLED setup fails, program will hang
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
         while (true)
             ;
+
+    // initialize accelgyro
+    if(!mpu.begin()) {
+      display.setTextSize(1);
+      display.setCursor(0, 0);
+      display.setTextColor(WHITE);
+      display.println("MPU6050 not initialized");
+      display.display();
+    }
+
+    // calibrate accelerometer
+    mpu.calcGyroOffsets();
+
 
     // set default OLED writing settings
     display.setTextSize(1);
@@ -109,8 +127,8 @@ void wait(uint16_t ms) {
 // function to write score to OLED display
 void display_score_to_oled() {
     // set cursor to first line and write score
-    display.setCursor(0, 0);
-    display.setTextSize(1);
+    display.setCursor(16, 0);
+    display.setTextSize(2);
     display.print("score: " + String(score));
     display.display();
 }
@@ -118,8 +136,8 @@ void display_score_to_oled() {
 // function to display command to OLED display
 void display_command_to_oled() {
     // set cursor to second line and write command
-    display.setCursor(0, 10);
-    display.setTextSize(1);
+    display.setCursor(0, 20);
+    display.setTextSize(2);
     display.print(commands_list[current_command]);
     display.display();
 }
@@ -131,41 +149,40 @@ void display_command_and_score_to_oled() {
     display_command_to_oled();
 }
 
-// void get_angle() {
-//   int xAng = map(ax, A_MIN, A_MAX, -90, 90);
-//   int yAng = map(ay, A_MIN, A_MAX, -90, 90);
-//   int zAng = map(az, A_MIN, A_MAX, -90, 90);
-
-//   X_ANG = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
-//   Y_ANG = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
-//   Z_ANG = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
-// }
-
 // functions to poll the sensors
 // the 1,2,4 method - no combination of them equals another and then we can distinguish between outputs passed back.
 int poll_twist_it() {
-    int twistVal = map(analogRead(TWIST_IT_ROT_POT), 0, 1023, 0, 179);
+    //TWIST_IT_VAL = map(analogRead(TWIST_IT_ROT_POT), 0, 1023, 0, 179);
+    TWIST_IT_VAL = analogRead(TWIST_IT_ROT_POT);
 
     // find difference between current val and prev val
-    int diff = abs(twistVal - PREV_TWIST_IT);
+    int diff = abs(TWIST_IT_VAL - PREV_TWIST_IT);
 
     // if difference is greater than tolerance, store old val and return correct
     if (diff > 100) {
-        PREV_TWIST_IT = twistVal;
+#ifdef DEBUG
+    display.clearDisplay();
+    display.println("Prev val" + String(PREV_TWIST_IT) + "curr: " + String(TWIST_IT_VAL));
+    display.display();
+    hold();
+#endif
+        // PREV_TWIST_IT = TWIST_IT_VAL;
         return 1;
     }
     return 0;
 }
 
 int poll_rip_it() {
-    int ripVal = map(analogRead(RIP_IT_SLIDE_POT), 0, 1023, 0, 179);
+    //RIP_IT_VAL = map(analogRead(RIP_IT_SLIDE_POT), 0, 1023, 0, 179);
+    RIP_IT_VAL = analogRead(RIP_IT_SLIDE_POT);
 
     // find difference between current val and prev val
-    int diff = abs(ripVal - PREV_RIP_IT);
+    int diff = abs(RIP_IT_VAL - PREV_RIP_IT);
 
     // if difference is greater than tolerance, store old val and return correct
     if (diff > TOLERANCE) {
-        PREV_RIP_IT = ripVal;
+
+        PREV_RIP_IT = RIP_IT_VAL;
         return 2;
     }
     return 0;
@@ -175,10 +192,14 @@ int poll_pour_it() {
     //accelgyro.getAcceleration(&ax, &ay, &az);
     // check not upright
 
-    // from how2electronics.com/measure-tilt-angle-mpu6050-arduino/
-    Z_ANG = mpu.getAngleZ();
+    // poll MPU 
+    mpu.update();
 
-    if(abs(Z_ANG) > POUR_IT_ANGLE) {
+    Z_ANG = mpu.getAngleZ();
+    Y_ANG = mpu.getAngleY();
+    X_ANG = mpu.getAngleX();
+
+    if(abs(X_ANG) > POUR_IT_ANGLE || abs(Y_ANG) > POUR_IT_ANGLE) {
       return 4;
     } else {
       return 0;
@@ -186,8 +207,7 @@ int poll_pour_it() {
 }
 
 int poll_sensors() {
-    return poll_rip_it() + poll_twist_it();
-    poll_pour_it();
+    return poll_rip_it() + poll_twist_it() + poll_pour_it();
 }
 
 // function to poll sensors and check if the user responded within the time limit
@@ -197,51 +217,58 @@ void wait_for_user_response(int command) {
     display_command_and_score_to_oled();
 
     // PREV_TWIST_IT = map(analogRead(TWIST_IT_ROT_POT), 0, 1023, 0, 179);
-    PREV_TWIST_IT = map(analogRead(TWIST_IT_ROT_POT), 0, 1023, 0, 179);
-    PREV_RIP_IT = map(analogRead(RIP_IT_SLIDE_POT), 0, 1023, 0, 179);
+    PREV_TWIST_IT = analogRead(TWIST_IT_ROT_POT);
+    PREV_RIP_IT = analogRead(RIP_IT_SLIDE_POT);
+
 
     // initialize the start time of the command being sent
-    int timeStart = millis();
-    int sensor_sum = poll_sensors();
+    wait(500);
+    SENSOR_SUM = poll_sensors();
+    TIMESTART = millis();
 
     // wait for input to go to desired and then back to normal state
-    int timeAction = timeStart;
+    int TIMEACTION = TIMESTART;
 
     // keep looping while time limit hasn't been reached or while no inputs have been selected
-    while ((sensor_sum == 0) && ((timeAction - timeStart) < inputTime * 1000)) {
+    while ((SENSOR_SUM == 0) && ((TIMEACTION - TIMESTART) < inputTime * 1000)) {
         // constantly poll time elapsed
-        timeAction = millis();
-        sensor_sum = poll_sensors();
+        TIMEACTION = millis();
+        // poll MPU values
+        SENSOR_SUM = poll_sensors();
     }
 
 #ifdef DEBUG
     display.clearDisplay();
     display.setCursor(0,0);
     display.setTextSize(1);
+    display.println(String(analogRead(TWIST_IT_ROT_POT)));
+    display.println(String(TWIST_IT_VAL) + " " + String(PREV_TWIST_IT));
     display.println(String(ax) + ", " + String(ay) + ", " + String(az));
-    display.println(String(X_ANG) + " " + String(X_ANG) + " " + String(X_ANG));
-    display.println(String(command) + "\n" + String(command_sum[command]) + "\n" + String(sensor_sum));
+    display.println(String(X_ANG) + " " + String(Y_ANG) + " " + String(Z_ANG));
+    display.println(String(command) + "\n" + String(command_sum[command]) + "\n" + String(SENSOR_SUM));
     display.display();
     hold();
 #endif
     
     // if time elapsed is longer than current input time allowed, game over!
-    if (sensor_sum != command_sum[command]) {
+    if (SENSOR_SUM != command_sum[command]) {
         // display score and game over message
         display.clearDisplay();
         display.setCursor(0, 0);
-        display.setTextSize(1);
-        display.print("score = " + String(score) + "\n");
-        display.setCursor(0, 10);
-        display.setTextSize(1);
-        display.print("GAME OVER!\n\n");
+        display.setTextSize(2);
+        display.println("score = " + String(score));
+        display.setCursor(0, 20);
+        display.setTextSize(2);
+        display.print("GAME OVER!");
 
         display.display();
-
+#if DEBUG
         while(digitalRead(START_BUTTON) == LOW);
         while(digitalRead(START_BUTTON) == HIGH);
+#endif
 
         // go back to start state
+        wait(2500);
         loop();
     } else {
         // if input was correct, increase score and decrease allowed input time
@@ -261,12 +288,17 @@ void loop() {
   // keep increasing the rand seed counter until start is pressed. This will add a randomness effect
   // because we don't have an RTC to keep track of time.
   while(digitalRead(START_BUTTON) == LOW) {
+    display.flush();
     display.clearDisplay();
-    display.setCursor((WIDTH / 2) - (String("BONK-IT!").length() / 2), 4);
+    display.setCursor((WIDTH / 4) -18, 4);
     display.setTextSize(2);
     display.println("BONK-IT!");
     display.setTextSize(1);
-    display.print("press lid to start");
+    display.setCursor(8, 20);
+    display.println("press lid to start");
+#if DEBUG
+    display.println("v0.9.5.3");
+#endif
     display.display();
     rand_seed_counter++;
   }
@@ -291,8 +323,7 @@ void loop() {
       delay(2000);
       while (score <= 99) {
         // get a random command
-        int command = rand() % 2;
-        //int command = 1;
+        int command = rand() % 3;
 
         // twist it
         if (command == TwistIt) {
@@ -319,5 +350,14 @@ void loop() {
           delay(1000);
         }
       }
+      // win screen
+      display.clearDisplay();
+      display.setCursor(4, 0);
+      display.setTextSize(2);
+      display.println("You win!");
+      display.display();
+      wait(5000);
+      // go back to start
+      loop();
   }
 }
